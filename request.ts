dc5ec95db47c051e50f73a1f2eac3ff327ef54d8
Sanjeev Mishra <mishragsanjeev1999@gmail.com>
2022-06-30 18:50:14 +0530

    some changes

--- request.ts
+++ request.ts
@@ -1,32 +1,34 @@
-/*const axios = require('axios')
+const axios = require('axios')
 const fs = require("fs");
 const { parse } = require("csv-parse");
 const CSVToJSON = require('csvtojson');
 
 const users = [];
-fs.createReadStream("./Customer.csv")
+fs.createReadStream("wallet.csv")
   .pipe(parse({ delimiter: ",", from_line: 2 }))
   .on("data", function (row) {
-//    // console.log(row);
-//     //console.log(row[0]);
-        users.push[row];
-        console.log(users)
-//     */let data =  
-//     "name": {
-//         "first": row[0],
-//         "last": row[1]
-//     },
-//     "authentication": {
-//         "securityQuestions": {
-//         "question": row[2],
-//         "answer": row[3]
-//         },
-//     "userName": row[4],
-//     "phone": row[5],
-//     "email": row[6],
-//     "password": row[7]
-//     }
-// }
+     console.log(row);
+     console.log(row[0]);
+       // users.push[row];
+      //  console.log(users)
+    let data = { 
+    "name": {
+        "first": row[0],
+        "last": row[1]
+    },
+    "authentication": {
+        "securityQuestions": {
+        "question": row[2],
+        "answer": row[3]
+        },
+    "userName": row[4],
+    "phone": row[5],
+    "email": row[6],
+    "password": row[7]
+    }
+   
+ }
+  console.log(data)
 //   //users.push(row)
     
 //     const options = {
@@ -45,10 +47,11 @@ fs.createReadStream("./Customer.csv")
   
 // console.log(data[1]);
 
- /* })
+
+  })
   .on("end", function () {
     console.log("finished");
   })
   .on("error", function (error) {
     console.log(error.message);
-  });*/
+  });
